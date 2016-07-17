320fd6c838448ffb2cceab8ec35ec02a52b11599
dogwood008 <dogwood008+github@gmail.com>
2016-07-17 20:09:35 +0900

    :heavy_minus_sign: remove columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710152551) do
+ActiveRecord::Schema.define(version: 20160717102707) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
     t.string   "url"
-    t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "date"
   end
 
 end
