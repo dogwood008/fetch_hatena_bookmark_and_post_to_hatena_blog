321f8b2c615df1c0fc93398e06bc493845b493de
dogwood008 <dogwood008+github@gmail.com>
2016-07-18 14:26:48 +0900

    :heavy_minus_sign: remove CRUD of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
   require 'rss'
   require 'open-uri'
 
-  before_action :set_post, only: [:show, :update, :destroy]
   before_action :fetch_feed, only: [:fetch, :fetch_and_post]
 
   def fetch
@@ -15,54 +14,7 @@ class PostsController < ApplicationController
     render json: post(not_yet_post_items)
   end
 
-  # GET /posts
-  def index
-    @posts = Post.all
-
-    render json: @posts
-  end
-
-  # GET /posts/1
-  def show
-    render json: @post
-  end
-
-  # POST /posts
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      render json: @post, status: :created, location: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      render json: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
-  end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def post_params
-    params.require(:post).permit(:title, :url, :comment)
-  end
 
   def fetch_feed
     url = 'http://b.hatena.ne.jp/dogwood008/rss'
