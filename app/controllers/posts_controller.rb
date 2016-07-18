fb35fe2c58ccec34d0ef10092b38a695c8dad1a4
dogwood008 <dogwood008+github@gmail.com>
2016-07-18 14:05:38 +0900

    :shirt: fix indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,37 +53,38 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-      params.require(:post).permit(:title, :url, :comment)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def fetch_feed
-      url = 'http://b.hatena.ne.jp/dogwood008/rss'
-      ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-      rss = open(url, 'User-Agent' => ua)
-      feed = RSS::Parser.parse(rss)
-      rss.close
-      @feeds = feed.items.map do |i|
-        { url: i.link, title: i.title, comment: i.description, date: i.dc_date }
-      end
-    end
+  # Only allow a trusted parameter "white list" through.
+  def post_params
+    params.require(:post).permit(:title, :url, :comment)
+  end
 
-    def get_already_post_index(already_post_url, feeds)
-      feeds.index { |f| f[:url] == already_post_url }
+  def fetch_feed
+    url = 'http://b.hatena.ne.jp/dogwood008/rss'
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+    rss = open(url, 'User-Agent' => ua)
+    feed = RSS::Parser.parse(rss)
+    rss.close
+    @feeds = feed.items.map do |i|
+      { url: i.link, title: i.title, comment: i.description, date: i.dc_date }
     end
+  end
 
-    def extract_not_yet_post_items(already_post_url, feeds)
-      already_post_index = get_already_post_index(already_post_url, feeds)
-      if already_post_index
-        feeds.values_at(Range.new(0, already_post_index - 1))
-      else
-        feeds
-      end
+  def get_already_post_index(already_post_url, feeds)
+    feeds.index { |f| f[:url] == already_post_url }
+  end
+
+  def extract_not_yet_post_items(already_post_url, feeds)
+    already_post_index = get_already_post_index(already_post_url, feeds)
+    if already_post_index
+      feeds.values_at(Range.new(0, already_post_index - 1))
+    else
+      feeds
     end
+  end
 end
